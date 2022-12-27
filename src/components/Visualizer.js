3df6cbc57b733a1eb125bb26fe2df6875e584120
codeeverydayyu <549117762@qq.com>
2022-12-26 19:11:15 -0800

    Make bubble sort process display on page, change color through the sorting process.

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,89 +1,150 @@
 import React, { useEffect, useState } from 'react';
 import './Visualizer.css';
 
+const UNSORTED_COLOR = 'gainsboro';
+const COMPARISON_COLOR = 'darkorange';
+const SORTED_COLOR = 'green';
+
 export default function Visualizer() {
+  const [arraySize, setArraySize] = useState(20);
   const [array, setArray] = useState([]);
+  const [speed, setSpeed] = useState(300);
 
   // generate a random array
   const randomArray = () => {
     let arr = [];
-    for (let i = 0; i < 10; i++) {
-      arr.push(Math.floor(Math.random() * 100) + 5);
+    for (let i = 0; i < arraySize; i++) {
+      arr.push(Math.floor(Math.random() * 200) + 5);
     }
     setArray(arr);
-    console.log('reset random array:');
-    console.log(array.toString());
+    // check if the element is null to avoid null error
+    let check = document.getElementById(0);
+    if (check !== null) {
+      for (let i = 0; i < arraySize; i++) {
+        // set the bars' color to unsorted color every time that generates a new random array
+        document.getElementById(i).style.backgroundColor = UNSORTED_COLOR;
+      }
+    }
   };
 
-  // update default array from random arrayfor the first time
+  // set random array for the first render
   useEffect(() => {
     randomArray();
   }, []);
-  // WHY use useEffect won't cause too many re-renders error?
 
-  const swap = (arr, xp, yp) => {
-    var temp = arr[xp];
-    arr[xp] = arr[yp];
-    arr[yp] = temp;
+  const fast = () => {
+    setSpeed(10);
+  };
+  const slow = () => {
+    setSpeed(300);
   };
 
-  // sorting algorithm
-  const bubbleSort = () => {
-    console.log('bubble sort');
-    console.log('unsorted array:');
-    console.log(array.toString());
-    var i, j;
-    for (i = 0; i < array.length - 1; i++) {
-      for (j = 0; j < array.length - i - 1; j++) {
+  // https://stackoverflow.com/questions/58816244/debugging-eslint-warning-function-declared-in-a-loop-contains-unsafe-reference
+  const sleep = (speed) => {
+    return new Promise((resolve) => setTimeout(resolve, speed));
+  };
+
+  const swap = (arr, x, y) => {
+    var temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+  };
+
+  // Bubble sort algorithm
+  const bubbleSort = async () => {
+    let bar1, bar2;
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = 0; j < array.length - i - 1; j++) {
+        // change the color of comparison element
+        bar1 = document.getElementById(j).style;
+        bar2 = document.getElementById(j + 1).style;
+        bar1.backgroundColor = COMPARISON_COLOR;
+        bar2.backgroundColor = COMPARISON_COLOR;
+
+        // swap elements and update array's changes to the state
         if (array[j] > array[j + 1]) {
           swap(array, j, j + 1);
+          setArray([...array, array]);
         }
+
+        // wait for a few milliseconds and then change color back to unsorted
+        await sleep(speed);
+        bar1.backgroundColor = UNSORTED_COLOR;
+        bar2.backgroundColor = UNSORTED_COLOR;
       }
+      // the element is in its sorted position, change color
+      bar2.backgroundColor = SORTED_COLOR;
     }
-    console.log('sorted array:');
-    console.log(array.toString());
+    // the element is in its sorted position, change color
+    bar1.backgroundColor = SORTED_COLOR;
   };
 
   // test results
   const testSort = () => {
-    console.log('test result:');
     let testResult = false;
-    const testArr = [...array];
-    testArr.sort();
-    if (testArr.length !== array.length) {
-      return;
-    } else {
+    const testArr = structuredClone(array);
+    console.log(testArr === array);
+    console.log('before using built-in sort');
+    console.log(testArr.toString());
+    testArr.sort((a, b) => a - b);
+    if (testArr.length === array.length) {
       for (let i = 0; i < testArr.length; i++) {
         if (testArr[i] !== array[i]) {
-          return;
+          break;
         }
         testResult = true;
       }
     }
-    console.log(testResult.toString());
+    console.log('test result: ', testResult.toString());
   };
 
   return (
     <div>
       Visualizer
+      <div className='colorSystem-container'>
+        <div
+          key={'unsorted'}
+          className='square'
+          style={{ background: UNSORTED_COLOR }}
+        >
+          Unsorted Elements
+        </div>
+        <div
+          key={'comparison'}
+          className='square'
+          style={{ background: COMPARISON_COLOR }}
+        >
+          Comparison Elements
+        </div>
+        <div
+          key={'soted'}
+          className='square'
+          style={{ background: SORTED_COLOR }}
+        >
+          Sorted Elements
+        </div>
+      </div>
       <div className='sortingBars-container'>
-        {array.map((element, index) => {
-          return (
-            <div
-              className='bar'
-              id={index}
-              key={index}
-              style={{
-                height: `${element}px`,
-              }}
-            ></div>
-          );
-        })}
+        {array &&
+          array.map((element, index) => {
+            return (
+              <div
+                className='bar'
+                id={index}
+                key={index}
+                style={{
+                  height: `${element}px`,
+                }}
+              ></div>
+            );
+          })}
       </div>
       <div className='button-container'>
-        <button onClick={randomArray}>Reset Array</button>
-        <button onClick={bubbleSort}>Bubble Sort</button>
+        <button onClick={randomArray}>Genarate random array</button>
+        <button onClick={bubbleSort}>Bubble sort</button>
         <button onClick={testSort}>Test</button>
+        <button onClick={fast}>Fast speed</button>
+        <button onClick={slow}>Slow speed</button>
       </div>
     </div>
   );
